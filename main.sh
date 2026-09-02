3e8daaee71f7626ca2773fa3bc8d5b55a688908a
Mateus Cruzatto <cruzatto@proton.me>
2026-09-02 13:43:37 -0300

    feat: add initial structure

---
+++ main.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+CMD_DIR="cmd"
+
+case "$1" in
+    build)
+	bash "$CMD_DIR/build.sh"
+        ;;
+    run)
+        bash "$CMD_DIR/run.sh"
+        ;;
+    clean)
+        bash "$CMD_DIR/clean.sh"
+	;;
+    rebuild)
+	bash "$CMD_DIR/rebuild.sh"
+	;;
+    info)
+	bash "$CMD_DIR/info.sh"
+	;;
+esac
