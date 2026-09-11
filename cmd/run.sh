0282d350adbf8f548d627ff1e1fd432df115119d
sarah <sarahantunes101@gmail.com>
2026-09-11 14:47:56 -0300

    feat: run command

--- cmd/run.sh
+++ cmd/run.sh
@@ -1,3 +1,9 @@
-# Erro: Tentativa de run sem executável disponível. (bash "$CMD_DIR/errors.sh" 7)
+# Erro: Tentativa de run sem executável disponível.
+if [[ ! -e $EXEC ]]; then
+	bash "$CMD_DIR/errors.sh" 7
+	exit 1
+fi
+
 # Erro: Permissões insuficientes. (bash "$CMD_DIR/errors.sh" 8)
-# ./$EXEC
+
+./$EXEC
