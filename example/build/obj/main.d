18596f792cae4a6f472a041e030c90310a249724
Mateus Cruzatto <cruzatto@proton.me>
2026-09-18 23:45:58 -0300

    feat: add report.pdf and README.md to docs/

--- example/build/obj/main.d
+++ example/build/obj/main.d
@@ -1,2 +1,4 @@
 /home/mateus/Projects/cbuild/example/build/obj/main.o: \
- /home/mateus/Projects/cbuild/example/src/main.c
+ /home/mateus/Projects/cbuild/example/src/main.c \
+ /home/mateus/Projects/cbuild/example/src/../include/add.h
+/home/mateus/Projects/cbuild/example/src/../include/add.h:
